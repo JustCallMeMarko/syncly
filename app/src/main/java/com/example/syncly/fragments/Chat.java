6d21ce61ff4f550a7a240a864864683a71a74acf
JustCallMeMarko <cantimbuhanmark.stnick@gmail.com>
2026-01-22 21:57:24 +0800

    aded functionality in chat

--- app/src/main/java/com/example/syncly/fragments/Chat.java
+++ app/src/main/java/com/example/syncly/fragments/Chat.java
@@ -1,7 +1,11 @@
 package com.example.syncly.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,6 +13,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.syncly.R;
+import com.example.syncly.activities.ChatUI;
+import com.example.syncly.layouts.NavigationLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -56,11 +62,25 @@ public class Chat extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+    ConstraintLayout profile;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        profile = view.findViewById(R.id.profile);
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ChatUI.class);
+                startActivity(intent);
+            }
+        });
     }
 }
